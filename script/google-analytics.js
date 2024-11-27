897ec8f56187817548e03f4b360450eb12f20a30
Anjali Justice <anjalijustice@gmail.com>
2024-11-27 09:18:34 -0500

    add google analytics test

---
+++ script/google-analytics.js
@@ -0,0 +1,7 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-QFSYEL50KL"></script>
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-QFSYEL50KL');
